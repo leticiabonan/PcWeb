60dd07773bf5048e586705471c73dffd97aa5028
leticiabonan <leticia.ls.bonan@gmail.com>
2025-04-19 20:26:59 -0300

    Update lab_string_atividade6.js

--- lab_string_atividade6.js
+++ lab_string_atividade6.js
@@ -1,1 +1,14 @@
-
+function escreverExtenso()
+{
+    let data = document.getElementById("data").value.split("-");
+    let meses = 
+    [
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    ];
+    let dia = parseInt(data[2]);
+    let mes = meses[parseInt(data[1]) - 1];
+    let ano = data[0];
+    let dataExtenso = (dia + " de " + mes + " de " + ano);
+    document.getElementById("saida").textContent = dataExtenso;
+}
